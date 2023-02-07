bba026a2f797e0150274e564a3ccd7ce748870d8
Chuck LU <chucklu2015@outlook.com>
2023-02-07 18:16:22 +0800

    define steps

---
+++ BaiduSample/SpecFlowProject1/StepDefinitions/BaiduAdvancedSearchSteps.cs
@@ -0,0 +1,51 @@
+using System;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowProject1.StepDefinitions
+{
+    [Binding]
+    public class BaiduAdvancedSearchSteps
+    {
+        [Given(@"打开百度首页")]
+        public void Given打开百度首页()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"鼠标悬停在“设置”按钮")]
+        public void When鼠标悬停在设置按钮()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"点击设置菜单上的“高级搜索“按钮")]
+        public void When点击设置菜单上的高级搜索按钮()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"弹出高级搜索页面")]
+        public void Then弹出高级搜索页面()
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"输入关键词""([^""]*)""")]
+        public void Given输入关键词(string p0)
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"点击高级搜索页面上的“高级搜索""按钮")]
+        public void When点击高级搜索页面上的高级搜索按钮()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"搜索框显示关键词""([^""]*)""")]
+        public void Then搜索框显示关键词(string p0)
+        {
+            throw new PendingStepException();
+        }
+    }
+}
