db21055ebc8d9929c5518b200ad9e6e641bd852f
Chuck LU <chucklu2015@outlook.com>
2023-02-08 17:45:19 +0800

    operate the web page

--- BaiduAdvancedSearch/BaiduAdvancedSearch/StepDefinitions/BaiduAdvancedSearchSteps.cs
+++ BaiduAdvancedSearch/BaiduAdvancedSearch/StepDefinitions/BaiduAdvancedSearchSteps.cs
@@ -4,13 +4,14 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using TechTalk.SpecFlow;
 using static System.Net.Mime.MediaTypeNames;
+using OpenQA.Selenium.Interactions;
 
 namespace SpecFlowProject1.StepDefinitions
 {
     [Binding]
     public class BaiduAdvancedSearchSteps
     {
-        private IWebDriver driver;
+        private static IWebDriver driver;
 
         [BeforeFeature]
         public static async Task BeforeFeature(IObjectContainer container)
@@ -18,7 +19,7 @@ namespace SpecFlowProject1.StepDefinitions
             ChromeOptions options = new ChromeOptions();
             options.AddArgument("--headless");
             options.AddArgument("--disable-gpu");
-            IWebDriver driver = new ChromeDriver(options);
+            driver = new ChromeDriver(options);
         }
 
         [Given(@"打开百度首页")]
@@ -31,37 +32,41 @@ namespace SpecFlowProject1.StepDefinitions
         public void When鼠标悬停在设置按钮()
         {
             IWebElement element = driver.FindElement(By.XPath("//*[@name='tj_settingicon' and @class='pf']"));
-            element.Click();
+            Actions action = new Actions(driver);
+            action.MoveToElement(element).Build().Perform();
         }
 
         [When(@"点击设置菜单上的“高级搜索“按钮")]
         public void When点击设置菜单上的高级搜索按钮()
         {
-            throw new PendingStepException();
+            IWebElement element = driver.FindElement(By.XPath("//*[contains(text(), '高级搜索')]"));
+            element.Click();
         }
 
         [Then(@"弹出高级搜索页面")]
         public void Then弹出高级搜索页面()
         {
-            throw new PendingStepException();
+
         }
 
         [Given(@"输入关键词""([^""]*)""")]
         public void Given输入关键词(string p0)
         {
-            throw new PendingStepException();
+            IWebElement element = driver.FindElement(By.CssSelector("input[name='q3'][class='c-input adv-q-input switch-input']"));
+            element.SendKeys("人生若只如初见");
         }
 
         [When(@"点击高级搜索页面上的“高级搜索""按钮")]
         public void When点击高级搜索页面上的高级搜索按钮()
         {
-            throw new PendingStepException();
+            IWebElement element = driver.FindElement(By.CssSelector("input.advanced-search-btn.c-btn.c-btn-primary.switch-button"));
+            element.Click();
         }
 
         [Then(@"搜索框显示关键词""([^""]*)""")]
         public void Then搜索框显示关键词(string p0)
         {
-            throw new PendingStepException();
+            //throw new PendingStepException();
         }
     }
 }
